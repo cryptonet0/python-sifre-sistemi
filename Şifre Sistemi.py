747e7cb58f29864b46f771d93adba17a5042226f
Herşey Hakkında <74898123+easyboy45@users.noreply.github.com>
2021-04-17 08:58:07 +0300

    Update Şifre Sistemi.py

--- Şifre Sistemi.py
+++ Şifre Sistemi.py
@@ -1,7 +1,7 @@
 print("Parolayı giriniz: ")
 user_name = input()
-if user_name == "M@t@":
-    print("Evin internet şifresi '4FF3ncqe6xTV'")
-    print("TC Kimlik Numarası: 21256393012")
+if user_name == "istediğiniz şey":
+    print("istediğiniz şey'")
+    print("istediğiniz şey")
 else:
     print("Parola Hatalı Lütfen Yeniden Deneyin")
