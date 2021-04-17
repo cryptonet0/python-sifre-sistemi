929d982b836e25720203c7034fe57825d5adcce8
Herşey Hakkında <74898123+easyboy45@users.noreply.github.com>
2021-04-17 08:45:06 +0300

    Add files via upload

---
+++ Şifre Sistemi.py
@@ -0,0 +1,7 @@
+print("Parolayı giriniz: ")
+user_name = input()
+if user_name == "M@t@":
+    print("Evin internet şifresi '4FF3ncqe6xTV'")
+    print("TC Kimlik Numarası: 21256393012")
+else:
+    print("Parola Hatalı Lütfen Yeniden Deneyin")
